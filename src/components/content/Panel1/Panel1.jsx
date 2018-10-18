e290b5f29a503359c68d3992069c36b8b83d501e
Gilberto Toledo <giltoram@gmail.com>
2018-10-18 00:39:31 -0500

    Created panel1

--- src/components/content/Panel1/Panel1.jsx
+++ src/components/content/Panel1/Panel1.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Image, Button, Grid, Row, Col   } from "react-bootstrap";
+import {Image, Button, Grid, Row, Col, FormControl, FormGroup } from "react-bootstrap";
 import './Panel1.scss';
 
 
@@ -7,7 +7,9 @@ class Panel1 extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
+        this.state = {
+            value: 'Your email address'
+        };
 
     }
 
@@ -22,6 +24,9 @@ class Panel1 extends React.Component {
                                 And create your own <br/>
                                 shopping cart
                             </text>
+                            <FormGroup controlId="demoRequest">
+                                <FormControl type="text" bsSize="large" value={this.state.value}/> <Button bsStyle="Basic">Request a demo</Button>
+                            </FormGroup>
                         </Col>
                         <Col xl={7}>
                             <Image src="assets/hero-mockup.png" className="panel1-image" responsive></Image>
